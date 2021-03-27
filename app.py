0948345b61ae56fb635effc8ffe1745f74de33b6
tuminzee <t.sheth33@gmail.com>
2021-03-27 14:53:48 +0530

    Integrate MongoDb, store predictions list in Db

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ from keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
 # Flask utils
-from flask import Flask, redirect, url_for, request, render_template
+import json
+from flask import Flask, redirect, url_for, request, render_template, jsonify, make_response
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from flask_bootstrap import Bootstrap
@@ -31,8 +32,55 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import SubmitField
 
+
+
+#addition by @tuminzee
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from skimage import io
+# import urllib
+# from skimage import io
+
 # Define a flask app
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client['SehatIntel']
+datas_collection = db['datas']
+users_collection = db['users']
+
+
+# app.config['MONGODB_SETTINGS'] = {
+#     'db': 'SehatIntel',
+#     'host': 'localhost',
+#     'port': 27017
+# }
+
+# db = MongoEngine()
+# db.init_app(app)
+# class User(db.Document):
+#     name = db.StringField()
+#     email = db.StringField()
+
+# class Users(db.Document):
+#     picture = db.StringField()
+#     name = db.StringField()
+
+# class Datas(db.Document):
+#     name: db.StringField()
+#     age: db.IntField()
+#     diseasesName: db.StringField()
+#     doctorsFeedback: db.StringField()
+#     doctorsName: db.StringField()
+#     email: db.StringField()
+#     hospitalsName: db.StringField()
+#     prescription: db.StringField()
+#     labReportFileId: db.StringField()
+#     labReportFileUrl: db.StringField()
+#     caseStartDate: db.DateTimeField()
+#     caseEndDate: db.DateTimeField()
+
+
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 bootstrap = Bootstrap(app)
 
@@ -54,10 +102,24 @@ class UploadForm(FlaskForm):
     ])
     submit = SubmitField('Classify')
 
+
+
+# def url_to_image(url):
+# 	# download the image, convert it to a NumPy array, and then read
+# 	# it into OpenCV format
+# 	resp = urllib.urlopen(url)
+# 	image = np.asarray(bytearray(resp.read()), dtype="uint8")
+# 	image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+# 	# return the image
+# 	return image
+
+
 def get_prediction(img_path):
 
     pred_label = []
-    img = cv2.imread(img_path)
+    # img = cv2.imread(img_path)
+    # img = url_to_image(img_path)
+    img = io.imread(img_path)
     img_resized = cv2.resize(img, (224, 224))
     img_preprocessed = preprocess_input(img_resized)
     img_reshaped = img_preprocessed.reshape((1, 224, 224, 3))
@@ -96,63 +158,66 @@ def home():
         prediction = None
     return render_template("index.html", form=form, file_url=file_url, prediction=prediction)
 
+
+@app.route('/image', methods=['POST', 'GET'])
+def pymongo_test():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        user_data = datas_collection.find_one({'_id': ObjectId(request_data['id'])})
+        if( user_data):
+            prediction = get_prediction(user_data['labReportFileUrl'])
+            filter = {
+                '_id':  ObjectId(request_data['id'])
+            }
+            new_values = {
+                "$set": {
+                    'labReportDiagnosistics': str(prediction)
+                }
+            }
+            datas_collection.update_one(filter, new_values)
+            return "data updated"
+        else:
+            return 'Try again with different object id'
+    else:
+        data = []
+        cursor  = datas_collection.find()
+        for doc in cursor:
+            doc['_id'] = str(doc['_id'])
+            data.append(doc)
+        return jsonify(data)
+
+
+
+# @app.route('/mongo', methods=['GET', 'POST'])
+# def mongodb_test():
+#     if request.method == 'GET':
+#         users = Users.objects()
+#         return (jsonify(users), 200)
+
+#     else:
+#         # record = json.loads(request.data)
+#         body = request.get_json()
+#         # return body
+#         user = User.objects(name=body['name']).first()
+#         if not user:
+#             return jsonify({'error': 'data not found'})
+#         else:
+#             user.update(email=body['email'])
+#         return jsonify(user.to_json())
+
+# @app.route('/image', methods=['GET', 'POST'])
+# def image_predict_from_mongodb():
+    # if request.method == 'POST':
+    #     request_data = request.get_json()
+    #     user_data = Datas.objects(_id=request_data['id']).first()
+    #     if not user_data:
+    #         return jsonify({'error': 'data not found'})
+    #     else:
+    #        return 'try again'
+    #     return jsonify(user.to_json())
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
 
-
-
-
-
-
-"""
-
-def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-
-    # Preprocessing the image
-    x = image.img_to_array(img)
-    # x = np.true_divide(x, 255)
-    x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    x = preprocess_input(x, mode='caffe')
-
-    preds = model.predict(x)
-    return preds
-
-
-@app.route('/', methods=['GET'])
-def index():
-    # Main page
-    return render_template('index.html')
-
-
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        # Get the file from post request
-        f = request.files['file']
-
-        # Save the file to ./uploads
-        basepath = os.path.dirname(__file__)
-        file_path = os.path.join(
-            basepath, '', secure_filename(f.filename))
-        f.save(file_path)
-
-        # Make prediction
-        preds = model_predict(file_path, model)
-
-        # Process your result for human
-        # pred_class = preds.argmax(axis=-1)            # Simple argmax
-        pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
-        result = str(pred_class[0][0][1])               # Convert to string
-        return result
-    return None
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""
